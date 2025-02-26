8ed6ad993f8661e6e7e6657bc3d4297ecf8266a0
Fatma Alghonaimy <ffatmaalaa32@gmail.com>
2025-02-26 03:30:47 +0200

    Temporary commit on master

--- Examination_System/Program.cs
+++ Examination_System/Program.cs
@@ -17,14 +17,15 @@ namespace Examination_System
             // To customize application configuration such as set high DPI settings or default font,
             // see https://aka.ms/applicationconfiguration.
             ApplicationConfiguration.Initialize();
-            //General.frmLogin = new frmLogin();
-            //Application.Run(General.frmLogin);
+            General.frmLogin = new frmLogin();
+            Application.Run(General.frmLogin);
+            //Application.Run(new frmAdmin());
 
 
             //Application.Run(new frmTeacherProfile());
             //Application.Run(new FormInsertQuestionsToExam(new Exam() {ID=1002, NoOfQuestions=9,CourseID=100 }));
             //Application.Run(new FormManageQuestions());
-            Application.Run(new FormAddExam());
+            //Application.Run(new FormAddExam());
         }
     }
 }
